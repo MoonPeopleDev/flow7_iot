e747c20fff67b0f0664f820c7830c375111635d4
Supergivi <chazoff@gmail.com>
2025-07-10 23:14:17 +0300

    s

--- app/models/devices/sensor.rb
+++ app/models/devices/sensor.rb
@@ -7,7 +7,7 @@ class Devices::Sensor < ApplicationRecord
   has_many :sensor_data_agg_10m, class_name: 'SensorData::Agg10m', foreign_key: :sensor_id
   has_many :sensor_data_agg_1h, class_name: 'SensorData::Agg1h', foreign_key: :sensor_id
   validates :name, presence: true
-  enum :algo, { base: 0, async: 1 }
+  enum :algo, { base: 0, async_algo: 1 }
   validates :cycle_threshold, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 600 }
 
   validate do |item|
