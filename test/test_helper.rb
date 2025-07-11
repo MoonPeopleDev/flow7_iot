381a0cd6ea57884c597725202df1f1b7eeec1c62
Cepega <chazoff@gmail.com>
2025-07-12 02:22:56 +0300

    Add time parsing helpers and tests

---
+++ test/test_helper.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'active_support/all'
+require 'action_controller/railtie'
+require 'active_record'
+
+# Minimal ApplicationController setup for tests
+module Api; module V1; end; end
+class ApplicationController < ActionController::Base
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :exception
+end
+
+require_relative '../app/controllers/api/v1/base_controller'
